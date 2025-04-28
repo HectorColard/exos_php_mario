ba64e2fc2a722c3d2375229d4268625d35587c0e
Hector Yunitto <hectorcolard@yunitto.fr>
2025-04-28 14:59:24 +0200

    initial commit

---
+++ exos/exo7.php
@@ -0,0 +1,41 @@
+<?php
+require_once '../inc/functions.php';
+
+/*
+ * Exo 7 : Mario DB!
+ * 
+ * (No class to write here)
+ * 
+ * 
+ *  1. Create a new database named 'supermario' in your localhost 
+ *     Import the SQL file sql/exo7-import.sql
+ * 
+ *  2. Establish a connection to this database from PHP, 
+ *     using the default user 'explorateur'.
+ *     Store this connection in the $pdo variable
+ * 
+ *  3. Find and store all heroes data in the $heroes variable
+ * 
+ *  4. Find and store all information of the hero named 'Yoshi' in the
+ *     $yoshi variable using an associative array
+ * 
+ *  5. Insert a new hero using these data :
+ *    - name : Princess Peach
+ *    - color : pink
+ *    - use default values for the others data
+ * 
+ */
+
+// ------------------------
+// START OF YOUR CODE
+// ------------------------
+
+// ------------------------
+// END OF YOUR CODE
+// ------------------------
+
+/*
+ * Tests
+ * Pas touche !
+ */
+check(7);
